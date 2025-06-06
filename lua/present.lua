d12e11b91da7b48c89295bdebb5d783ae27da14f
Bhavya Jain <bhavya@stackgen.com>
2025-06-07 00:08:00 +0530

    checkpoint 2

--- lua/present.lua
+++ lua/present.lua
@@ -1,55 +1,47 @@
 local M = {}
 
-local function create_floating_window(opts)
-  opts = opts or {}
-  local width = opts.width or math.floor(vim.o.columns * 0.8)
-  local height = opts.height or math.floor(vim.o.lines * 0.8)
-
-  -- Calculate the position to center the window
-  local row = math.floor((vim.o.lines - height) / 2)
-  local col = math.floor((vim.o.columns - width) / 2)
-
+local function create_floating_window(config)
   -- Create a buffer for the floating window
   local buf = vim.api.nvim_create_buf(false, true) -- false: not listed, true: scratch buffer
 
-  -- Define window configuration
-  --- @type vim.api.keyset.win_config
-  local win_config = {
-    relative = "editor",
-    width = width,
-    height = height,
-    row = row,
-    col = col,
-    style = "minimal",
-    border = "rounded", -- or "single", "double", "solid", "shadow"
-  }
-
   -- Create the floating window
-  local win = vim.api.nvim_open_win(buf, true, win_config)
+  local win = vim.api.nvim_open_win(buf, true, config)
 
   return { buf = buf, win = win }
 end
 
 --- @class present.Slides
---- @field slides string[]: the slides in the presentation
+--- @field slides present.Slide[]: the slides in the presentation
+
+--- @class present.Slide
+--- @field title string: the title of the slide
+--- @field body string[]: the body of the slide
 
 --- Takes some lines and parses them
 --- @param lines string[]: lines in the buffer
 --- @return present.Slides
 local parse_slides = function(lines)
   local slides = { slides = {} }
-  local current_slide = {}
+  local current_slide = {
+    title = "",
+    body = {},
+  }
 
   local separator = "^#"
 
   for _, line in ipairs(lines) do
     if line:find(separator) then
       -- If we have a current slide, save it before starting a new one
-      if #current_slide > 0 then
+      if #current_slide.title > 0 then
         table.insert(slides.slides, current_slide)
       end
 
-      current_slide = {}
+      current_slide = {
+        title = line,
+        body = {},
+      }
+    else
+      table.insert(current_slide.body, line)
     end
     -- Continue adding lines to the current slide
     table.insert(current_slide, line)
@@ -65,30 +57,103 @@ M.start_presentation = function(opts)
 
   local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
   local parsed_slides = parse_slides(lines)
-  local floating_window = create_floating_window()
 
-  ---@diagnostic disable-next-line: param-type-mismatch
-  vim.api.nvim_buf_set_lines(floating_window.buf, 0, -1, false, parsed_slides.slides[1])
+  local width = vim.o.columns
+  local height = vim.o.lines
+  --- @type vim.api.keyset.win_config[]
+  local windows = {
+    background = {
+      relative = "editor",
+      width = width,
+      height = height,
+      row = 0,
+      col = 0,
+      style = "minimal",
+      zindex = 1,
+    },
+    header = {
+      relative = "editor",
+      width = width,
+      height = 1,
+      row = 0,
+      col = 1,
+      style = "minimal",
+      border = "rounded",
+      zindex = 2,
+    },
+    body = {
+      relative = "editor",
+      width = width - 8,
+      height = height - 5, -- Leave space for header and footer
+      row = 4,
+      col = 8,
+      style = "minimal",
+      border = { " ", " ", " ", " ", " ", " ", " ", " " },
+      zindex = 2,
+    },
+    -- footer = {},
+  }
+
+  local background_floating_window = create_floating_window(windows.background)
+  local header_floating_window = create_floating_window(windows.header)
+  local body_floating_window = create_floating_window(windows.body)
+
+  vim.bo[header_floating_window.buf].filetype = "markdown"
+  vim.bo[body_floating_window.buf].filetype = "markdown"
+
+  local set_slide_content = function(idx)
+    local slide = parsed_slides.slides[idx]
+
+    local title = slide.title
+    local padding = string.rep(" ", math.floor((width - #title) / 2))
+    vim.api.nvim_buf_set_lines(header_floating_window.buf, 0, -1, false, { padding .. title })
+    vim.api.nvim_buf_set_lines(body_floating_window.buf, 0, -1, false, slide.body)
+  end
 
   local slide_index = 1
   vim.keymap.set("n", "n", function()
     slide_index = math.min(slide_index + 1, #parsed_slides.slides)
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.api.nvim_buf_set_lines(floating_window.buf, 0, -1, false, parsed_slides.slides[slide_index])
-  end, { buffer = floating_window.buf })
+    set_slide_content(slide_index)
+  end, { buffer = body_floating_window.buf })
 
   vim.keymap.set("n", "p", function()
     slide_index = math.max(slide_index - 1, 1)
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.api.nvim_buf_set_lines(floating_window.buf, 0, -1, false, parsed_slides.slides[slide_index])
-  end, { buffer = floating_window.buf })
+    set_slide_content(slide_index)
+  end, { buffer = body_floating_window.buf })
 
   vim.keymap.set("n", "q", function()
-    vim.api.nvim_win_close(floating_window.win, true)
-  end, { buffer = floating_window.buf })
+    vim.api.nvim_win_close(body_floating_window.win, true)
+  end, { buffer = body_floating_window.buf })
+
+  local restore = {
+    cmdheight = {
+      original = vim.o.cmdheight,
+      present = 0,
+    },
+  }
+
+  -- Set options for the presentation
+  for option, config in pairs(restore) do
+    vim.opt[option] = config.present
+  end
+
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = body_floating_window.buf,
+    callback = function()
+      -- Restore original options when leaving the presentation
+      for option, config in pairs(restore) do
+        vim.opt[option] = config.original
+      end
+
+      pcall(vim.api.nvim_win_close, header_floating_window.win, true)
+      pcall(vim.api.nvim_win_close, background_floating_window.win, true)
+    end,
+  })
+
+  set_slide_content(slide_index) -- Set the initial slide content
 end
 
--- M.start_presentation { bufnr = 63 }
+-- M.start_presentation { bufnr = 14 }
 
 -- vim.print(parse_slides {
 --   "# Hello",
--- test/test-markdown.md
+++ test/test-markdown.md
@@ -4,8 +4,12 @@ This is a test markdown file.
 
 # World
 
-This is another section of the markdown file.
+- This is another section of the markdown file.
+- It contains a list item.
 
 # Last
 
-This is the last section of the markdown file.
+```lua
+print("This is a Lua code block")
+print("It has multiple lines")
+```
